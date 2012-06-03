65196022a966ab880269f8d28cd8e73304b49daf
FelipeSenra <cutterofbones@gmail.com>
2012-06-03 17:27:16 -0300

    Update master

--- Control/Control.cpp
+++ Control/Control.cpp
@@ -3,22 +3,35 @@
 using namespace controlens;
 using namespace std;
 
+/*
+* habilita o print
+*/
 void Control::enablePrint(){
 
 	controlens::Control::getInstance()->toPrint =true;
 }
-
+/*
+* desabilita o print
+*/
 void Control::disablePrint(){
 	Control::instance->toPrint = false ;
 }
+/*
+* Construtor do Controle
+*/
 Control::Control()
 {
    this->toPrint = false ;
 }
+/*
+Retorna se deve ou nao printar os resultados
+*/
 bool Control::ToPrint(){
   return Control::getInstance().toPrint;
 }
-
+/*
+retorna a instancia de controle se caso existir ou cria e retorna essa instancia caso nao
+*/
  Control Control::getInstance(){
      if (!instance) instance = new Control();
         return *instance;
